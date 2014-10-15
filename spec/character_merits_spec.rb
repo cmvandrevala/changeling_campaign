6dce9697cedd3c35706c14c07fe90cbe3d521ebc
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2014-10-15 11:59:14 -0400

    Add merit class

---
+++ spec/character_merits_spec.rb
@@ -0,0 +1,29 @@
+require "character_merits"
+
+describe CharacterMerits do
+
+  before(:each) do
+    @merit = CharacterMerits.new
+  end
+
+  it "begins with no merits" do
+    expect(@merit.list_all).to eql []
+  end
+
+  it "you can add a merit" do
+    @merit.add_merit("Thaumaturgy", 4)
+    expect(@merit.list_all).to eql [{thaumaturgy: 4}]
+  end
+
+  it "properly handles spaces" do
+    @merit.add_merit("Thaumaturgy Extreme", 1)
+    expect(@merit.list_all).to eql [{thaumaturgy_extreme: 1}]
+  end
+
+  it "does not allow duplicates" do
+    @merit.add_merit("Thaumaturgy", 1)
+    @merit.add_merit("Thaumaturgy", 2)
+    expect(@merit.list_all).to eql [{thaumaturgy: 1}]
+  end
+
+end
