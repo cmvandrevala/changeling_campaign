ee4e6c7729e6ab1ab4df5866ac5437502af77038
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2014-12-23 18:40:56 -0500

    Update seeds

--- db/seeds/locations/locations.rb
+++ db/seeds/locations/locations.rb
@@ -1,11 +1,38 @@
+puts "===> New York City"
+
+Location.create({
+  name: "Lower East Side",
+  description: "The Lower East Side is a neighborhood in the southeastern part of the New York City borough of Manhattan, roughly located between the Bowery and the East River, and Canal Street and Houston Street. It has become a home to upscale boutiques and trendy dining establishments along Clinton Street's restaurant row.",
+  address: "New York City, USA"
+})
+
+Location.create({
+  name: "Columbia University",
+  description: "Columbia University is an American private Ivy League research university located in the Morningside Heights neighborhood of Upper Manhattan in New York City. It is the oldest institution of higher learning in the State of New York, the fifth oldest in the United States, and one of the country's nine Colonial Colleges founded before the American Revolution.",
+  address: "New York City, USA"
+})
+
 Location.create({name: "", description: "", address: "Chicago, USA"})
 Location.create({name: "Rajendra Nagar Slum", description: "", address: "Bangalore, India"})
-Location.create({name: "Arcadia", description: "", address: ""})
 
-puts "===> Hedge Locations"
+
+puts "===> The Hedge"
+
 Location.create({name: "The North Star", description: "", address: "The Hedge"})
 Location.create({name: "The Lacey Fair", description: "", address: "The Hedge"})
 Location.create({name: "The Briars", description: "", address: "The Hedge"})
+
+Location.create({
+  name: "Adi's Palace",
+  description: "A handsome estate created by Adi and her fetch.",
+  address: "The Hedge"
+})
+
+Location.create({
+  name: "Arcadia",
+  description: "The city of the True Fae. Little is known about this strange place.",
+  address: "The Hedge"
+})
 
 Location.create({
   name: "The Dual Forest",
