9a4059991ec4ab191e5bc07885b161efe827e533
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2014-12-24 16:26:42 -0500

    Add information from DropBox to repo

--- db/seeds/events/history_of_the_briars.rb
+++ db/seeds/events/history_of_the_briars.rb
@@ -7,3 +7,8 @@ Event.create({
 })
 Event.last.characters << Character.where(name: "Neithan").take
 Event.last.characters << Character.where(name: "Gon").take
+
+
+# The Briars are a piece of pseudo-wild territory within The Hedge that houses a number of outlaws.  This land is very similar to Brazenwood in Lost Girl.  That is to say, generally, people can do whatever they want.  The sheriff of the land is only there to mediate disputes - not to solve any sort of actual crime.
+#
+# All disputes are solved either by one party backing down or a Hedge Duel.
