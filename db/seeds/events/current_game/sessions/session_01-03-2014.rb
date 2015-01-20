715f4df481a9b4ca89037c82dc6fb6c627f81d71
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2015-01-20 18:48:46 -0500

    Add events from first session

--- db/seeds/events/current_game/sessions/session_01-03-2014.rb
+++ db/seeds/events/current_game/sessions/session_01-03-2014.rb
@@ -1,2 +1,12 @@
 puts "===> Session #1 - March 1, 2014"
 
+Event.create({
+  name: "The First Meeting",
+  date: Date.new(2014,03,01),
+  description: "The party meets up in the freehold. Immediately afterwards they travel to the Lacey Fair and shop for a hollow."
+})
+Event.last.characters << Character.where(name: "Lumi Satala").take
+Event.last.characters << Character.where(name: "Nola McNamara").take
+Event.last.characters << Character.where(name: "Jack Wedgewood").take
+Event.last.characters << Character.where(name: "Johannas").take
+Location.where(name: "The Lacey Fair").take.events << Event.last
