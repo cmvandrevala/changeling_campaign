03f35fef21f6ce80aa8c6e29749d706cfaadfb66
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2015-01-20 18:45:29 -0500

    Finish events for first downtime

--- db/seeds/events/current_game/downtimes/downtime_1.rb
+++ db/seeds/events/current_game/downtimes/downtime_1.rb
@@ -19,6 +19,14 @@ Event.last.characters << Character.where(name: "Neithan").take
 Location.where(name: "The Lacey Fair").take.events << Event.last
 
 Event.create({
+  name: "A New Hollow",
+  date: Date.new(2014,03,02),
+  description: "Jack signs the contract to rent the party's hollow."
+})
+Event.last.characters << Character.where(name: "Jack Wedgewood").take
+Location.where(name: "The Lacey Fair").take.events << Event.last
+
+Event.create({
   name: "The Dream",
   date: Date.new(2014,03,03),
   description: "Lumi has a prophetic dream about eight figures. In the process, she makes pseudo-permanent changes to the party's potential Hollow."
@@ -28,8 +36,17 @@ Location.where(name: "Dreamscape").take.events << Event.last
 
 Event.create({
   name: "New Clothes",
-  date: Date.new(2014,03,8),
+  date: Date.new(2014,03,7),
   description: "Lumi steals a few outfits and makeup for her new life as a Changeling."
 })
 Event.last.characters << Character.where(name: "Lumi Satala").take
 Location.where(name: "Anansesem Shopping District").take.events << Event.last
+
+Event.create({
+  name: "Flirting",
+  date: Date.new(2014,03,7),
+  description: "Nola tries to flirt with Jack and invite him back to her place. He almost decides to go, but catches himself at the last minute and excuses himself."
+})
+Event.last.characters << Character.where(name: "Nola McNamara").take
+Event.last.characters << Character.where(name: "Jack Tallow").take
+Location.where(name: "The Front Gates").take.events << Event.last
