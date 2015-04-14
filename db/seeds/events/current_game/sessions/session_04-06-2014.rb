dbf2df12e6aebeeef337577595d301a1b487accc
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2015-04-14 17:15:12 -0400

    Add events from session #11

--- db/seeds/events/current_game/sessions/session_04-06-2014.rb
+++ db/seeds/events/current_game/sessions/session_04-06-2014.rb
@@ -1,1 +1,23 @@
 puts "===> Sessions #10-11 - April 6, 2014"
+
+Event.create({
+  name: "The Banner",
+  date: Date.new(2014,04,06),
+  description: "The team manages to plant a banner on the World Tree. However, in the process, Jack is caught."
+})
+Event.last.characters << Character.where(name: "Lumi Satala").take
+Event.last.characters << Character.where(name: "Issac Corvi").take
+Event.last.characters << Character.where(name: "Nola MacNamara").take
+Event.last.characters << Character.where(name: "Jack Wedgewood").take
+Location.where(name: "The World Tree").take.events << Event.last
+
+Event.create({
+  name: "Lockdown",
+  date: Date.new(2014,04,07),
+  description: "The rulers of the freehold order a lockdown in order to find the culprits who posted the banner on the tree."
+})
+Event.last.characters << Character.where(name: "Elizabeth Ward").take
+Event.last.characters << Character.where(name: "Fredrick Ackerley").take
+Event.last.characters << Character.where(name: "The Jester").take
+Event.last.characters << Character.where(name: "Aldric Durant").take
+Location.where(name: "The World Tree").take.events << Event.last
