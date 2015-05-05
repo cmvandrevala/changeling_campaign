b825a5cedb39fe225c5b2105f4be5f112fba175e
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2015-05-05 16:32:28 -0400

    Add events for session #14

--- db/seeds/events/current_game/sessions/session_04-21-2014.rb
+++ db/seeds/events/current_game/sessions/session_04-21-2014.rb
@@ -1,13 +1,13 @@
-puts "===> Sessions #13 - April 21, 2014"
+puts "===> Sessions #13-14 - April 21, 2014"
 
 Event.create({
   name: "The Spokeswoman",
   date: Date.new(2014,4,21),
-  description: "The team decides to tail Chloe Ford in order to decide if she is right for the job of spokeswoman for the Anemoi Quartet."
+  description: "The team decides to tail Chloe Ford in order to decide if she is right for the job of spokeswoman for the Anemoi Quartet. Lumi and Nola spend the afternoon picking mushrooms with her, learning about her personality."
 })
 Event.last.characters << Character.where(name: "Lumi Satala").take
 Event.last.characters << Character.where(name: "Chloe Ford").take
 Event.last.characters << Character.where(name: "Nola MacNamara").take
 Event.last.characters << Character.where(name: "Jack Wedgewood").take
 Event.last.characters << Character.where(name: "Johannas").take
-Location.where(name: "The Notley's Hollow").take.events << Event.last
+Location.where(name: "Anansesem Forest Preserve").take.events << Event.last
